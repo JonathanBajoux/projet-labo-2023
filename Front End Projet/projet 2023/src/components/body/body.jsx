e5c1197e2d069e43a93247425d60913a9cf43b47
bajoux <jonathanbajoux.pro@gmail.com>
2023-12-07 16:30:32 +0100

    modif Front End

--- Front End Projet/projet 2023/src/components/body/body.jsx
+++ Front End Projet/projet 2023/src/components/body/body.jsx
@@ -4,30 +4,32 @@ import './body.scss';
 const Body = () => {
 
     return (
-        <main>
-            <div className='img'>
-                <section className='article-jeux'>
-                    <article className='article1'>
-                        <p>la structure va contenir l'article du site(partie 1)</p>
-                    </article>
-                    <article className='article2'>
-                        <p>la structure va contenir l'article du site(partie 2)</p>
-                    </article>
-                    <article className='article3'>
-                        <p>la structure va contenir l'article du site(partie 3)</p>
-                    </article>
-                    <article className='article4'>
-                        <p>la structure va contenir l'article du site(partie 4)</p>
-                    </article>
-                    <article className='article5'>
-                        <p>la structure va contenir l'article du site(partie 5)</p>
-                    </article>
-                    <article className='article6'>
-                        <p>la structure va contenir l'article du site(partie 6)</p>
-                    </article>
-                </section>
-            </div>
-        </main>
+        <body>
+            <main >
+                <div className='img'>
+                    <section className='article-jeux'>
+                        <article className='article1'>
+                            <p>la structure va contenir l'article du site(partie 1)</p>
+                        </article>
+                        <article className='article2'>
+                            <p>la structure va contenir l'article du site(partie 2)</p>
+                        </article>
+                        <article className='article3'>
+                            <p>la structure va contenir l'article du site(partie 3)</p>
+                        </article>
+                        <article className='article4'>
+                            <p>la structure va contenir l'article du site(partie 4)</p>
+                        </article>
+                        <article className='article5'>
+                            <p>la structure va contenir l'article du site(partie 5)</p>
+                        </article>
+                        <article className='article6'>
+                            <p>la structure va contenir l'article du site(partie 6)</p>
+                        </article>
+                    </section>
+                </div>
+            </main>
+        </body>
     );
 
 };
--- Front End Projet/projet 2023/src/components/navbar/navbar.jsx
+++ Front End Projet/projet 2023/src/components/navbar/navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './navbar.scss';
+import logoImage from '../../assets/logo/Player_One__1_-removebg-preview.png';
 
 const NavBar = () => {
 
     return (
         <div className='background1'>
+            <img className='Logo' src={logoImage} />
             <nav className='bouton-container'>
-                <input type="search" name="rechercher" id="search" />
+                <input type="search" name="recherche" id="search" />
                 <button className='types-bouton1'> console </button>
                 <button className='types-bouton2'> jeux vidéo </button>
                 <button className='types-bouton3'> goodies </button>
             </nav >
         </div >
+
     );
 
 };
