c35effeadcc6297dcea763f043125cf84f58612c
bajoux <jonathanbajoux.pro@gmail.com>
2023-12-08 12:06:02 +0100

    modif Front End

--- Front End Projet/projet 2023/src/components/body/body.jsx
+++ Front End Projet/projet 2023/src/components/body/body.jsx
@@ -8,24 +8,38 @@ const Body = () => {
             <main >
                 <div className='img'>
                     <section className='article-jeux'>
-                        <article className='article1'>
-                            <p>la structure va contenir l'article du site(partie 1)</p>
-                        </article>
-                        <article className='article2'>
-                            <p>la structure va contenir l'article du site(partie 2)</p>
-                        </article>
-                        <article className='article3'>
-                            <p>la structure va contenir l'article du site(partie 3)</p>
-                        </article>
-                        <article className='article4'>
-                            <p>la structure va contenir l'article du site(partie 4)</p>
-                        </article>
-                        <article className='article5'>
-                            <p>la structure va contenir l'article du site(partie 5)</p>
-                        </article>
-                        <article className='article6'>
-                            <p>la structure va contenir l'article du site(partie 6)</p>
-                        </article>
+                        <ul className='ul-article'>
+                            <li>
+                                <article className='article1'>
+                                    <p>la structure va contenir l'article du site(partie 1)</p>
+                                </article>
+                            </li>
+                            <li>
+                                <article className='article2'>
+                                    <p>la structure va contenir l'article du site(partie 2)</p>
+                                </article>
+                            </li>
+                            <li>
+                                <article className='article3'>
+                                    <p>la structure va contenir l'article du site(partie 3)</p>
+                                </article>
+                            </li>
+                            <li>
+                                <article className='article4'>
+                                    <p>la structure va contenir l'article du site(partie 4)</p>
+                                </article>
+                            </li>
+                            <li>
+                                <article className='article5'>
+                                    <p>la structure va contenir l'article du site(partie 5)</p>
+                                </article>
+                            </li>
+                            <li>
+                                <article className='article6'>
+                                    <p>la structure va contenir l'article du site(partie 6)</p>
+                                </article>
+                            </li>
+                        </ul>
                     </section>
                 </div>
             </main>
