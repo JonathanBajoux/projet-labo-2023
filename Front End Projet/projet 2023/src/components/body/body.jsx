4fc4fc37204480d899b2ab15266c44c3acea616d
bajoux <jonathanbajoux.pro@gmail.com>
2023-12-13 09:47:15 +0100

    modif front end

--- Front End Projet/projet 2023/src/components/body/body.jsx
+++ Front End Projet/projet 2023/src/components/body/body.jsx
@@ -6,42 +6,42 @@ const Body = () => {
     return (
         <body>
             <main >
-                <div className='img'></div>
-                <section >
-                    <ul className='article-jeux'>
-                        <li className='li-article1'>
-                            <article className='article1'>
-                                <p>la structure va contenir l'article du site(partie 1)</p>
-                            </article>
-                        </li>
-                        <li className='li-article2'>
-                            <article className='article2'>
-                                <p>la structure va contenir l'article du site(partie 2)</p>
-                            </article>
-                        </li>
-                        <li className='li-article3'>
-                            <article className='article3'>
-                                <p>la structure va contenir l'article du site(partie 3)</p>
-                            </article>
-                        </li>
-                        <li className='li-article4'>
-                            <article className='article4'>
-                                <p>la structure va contenir l'article du site(partie 4)</p>
-                            </article>
-                        </li>
-                        <li className='li-article5'>
-                            <article className='article5'>
-                                <p>la structure va contenir l'article du site(partie 5)</p>
-                            </article>
-                        </li>
-                        <li className='li-article6'>
-                            <article className='article6'>
-                                <p>la structure va contenir l'article du site(partie 6)</p>
-                            </article>
-                        </li>
-                    </ul>
-                </section>
-
+                <div className='img'>
+                    <section>
+                        <ul className='article-jeux'>
+                            <li className='li-article1'>
+                                <article className='article1'>
+                                    <p>la structure va contenir l'article du site(partie 1)</p>
+                                </article>
+                            </li>
+                            <li className='li-article2'>
+                                <article className='article2'>
+                                    <p>la structure va contenir l'article du site(partie 2)</p>
+                                </article>
+                            </li>
+                            <li className='li-article3'>
+                                <article className='article3'>
+                                    <p>la structure va contenir l'article du site(partie 3)</p>
+                                </article>
+                            </li>
+                            <li className='li-article4'>
+                                <article className='article4'>
+                                    <p>la structure va contenir l'article du site(partie 4)</p>
+                                </article>
+                            </li>
+                            <li className='li-article5'>
+                                <article className='article5'>
+                                    <p>la structure va contenir l'article du site(partie 5)</p>
+                                </article>
+                            </li>
+                            <li className='li-article6'>
+                                <article className='article6'>
+                                    <p>la structure va contenir l'article du site(partie 6)</p>
+                                </article>
+                            </li>
+                        </ul>
+                    </section>
+                </div>
             </main>
         </body >
     );
